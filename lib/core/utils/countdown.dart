3d8944498d7457d08e5d68eafc7eea52b9b0c2e4
letha11 <ibkaanhar1@gmail.com>
2023-05-04 16:04:09 +0700

    refactor(Countdown): change `.count` method to take tick+1 to make the method emit `0`

--- lib/core/utils/countdown.dart
+++ lib/core/utils/countdown.dart
@@ -9,7 +9,8 @@ class Countdown {
     if (tick < 0) return Left(FormatFailure());
 
     return Right(
-      Stream<int>.periodic(const Duration(seconds: 1), (count) => tick - count).take(tick),
+      Stream<int>.periodic(const Duration(seconds: 1), (count) => tick - count)
+          .take(tick + 1), // +1 because the tick starts from 0
     );
   }
 }
--- test/timer/core/utils/countdown_test.dart
+++ test/timer/core/utils/countdown_test.dart
@@ -1,4 +1,5 @@
 import 'package:dartz/dartz.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:pomodoro_timer/core/exceptions/failures.dart';
 import 'package:pomodoro_timer/core/utils/countdown.dart';
@@ -6,22 +7,28 @@ import 'package:pomodoro_timer/core/utils/countdown.dart';
 void main() {
   group('count()', () {
     late Countdown countdown;
-
     setUp(() {
       countdown = const Countdown();
     });
 
     test(
         'should count from given tick, to 0 and emit every 1 second when the seconds is not below - point',
-        () {
-      const seconds = 2;
+        () async {
+      const seconds = 3;
+      const countdown = Countdown();
       final stream = countdown.count(seconds);
 
-      expect(stream, isA<Right>());
-      expectLater((stream as Right).value, emitsInOrder([2, 1]));
+      await stream.fold(
+        (l) => fail('failed'),
+        (r) async {
+          expect(r, isA<Stream<int>>());
+          expect(r, emitsInOrder([3, 2, 1, 0]));
+        },
+      );
     });
 
-    test('should return Left(Failure) when the duration/seconds is below -', () {
+    test('should return Left(Failure) when the duration/seconds is below -',
+        () {
       const seconds = -1;
       final stream = countdown.count(seconds);
 
